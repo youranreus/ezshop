c7e81c8e87c12bdb91797ae1e76cb0b1d44f8915
youranreus <youranreus@qq.com>
2022-08-08 19:59:58 +0800

    feat: 后台页面基本框架

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -2,11 +2,42 @@
  * @author 季悠然
  * @date 2022-08-08
  */
+import { Layout, Col, Row, Typography } from '@douyinfe/semi-ui';
+import {Outlet} from "react-router-dom";
+import '../style/admin.scss';
 
 export default function Admin() {
+    const {Header, Content, Footer} = Layout;
+    const {Text} = Typography;
+
     return (
-        <div>
-            <h2>这里是后台</h2>
+        <div className={"admin"}>
+            <Layout>
+                <Header>
+                    <div className="header">
+                        <div style={{margin: "0 0.5rem"}}>
+                            <Row>
+                                <Col span={12}>礼品Go 后台管理</Col>
+                                <Col span={12} style={{textAlign: "right"}}>
+                                    <Text link={{href: "/"}}>返回首页</Text>
+                                </Col>
+                            </Row>
+                        </div>
+                    </div>
+                </Header>
+                <Content>
+                    <div style={{marginTop: "3rem"}}>
+                        <Outlet/>
+                    </div>
+                </Content>
+                <Footer>
+                    <div className="footer">
+                        <div style={{margin: "0 0.5rem"}}>
+                            <p>made with ❤ by SZTU ACM</p>
+                        </div>
+                    </div>
+                </Footer>
+            </Layout>
         </div>
     )
 }
