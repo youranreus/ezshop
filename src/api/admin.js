d85e520d3cb3b708f0084ce7014e993fe9a45786
youranreus <youranreus@qq.com>
2022-08-09 17:12:47 +0800

    feat: 物品删除功能

--- src/api/admin.js
+++ src/api/admin.js
@@ -5,7 +5,12 @@
 
 import API from "./index";
 import {APIWithAuth} from "./index";
+import {getQueryJson} from "../utils";
 
 export const login = (username, password) => API.post('/token', {username, password});
 
 export const AddGift = (data) => APIWithAuth.post('/thing', data);
+
+export const QueryThingList = (json) => API.get(`/thing_list${json ? '?json='+json : ''}`)
+
+export const DelGift = id => APIWithAuth.delete('/thing?json='+ getQueryJson({id: id}));
--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -4,16 +4,98 @@
  */
 import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
-import {IconPlus} from "@douyinfe/semi-icons";
-import {Toast, Typography, Button, ButtonGroup, Col, Row, Space, SideSheet, Form} from "@douyinfe/semi-ui";
-import {AddGift} from "../../api/admin";
+import {IconPlus, IconDelete, IconMinus} from "@douyinfe/semi-icons";
+import {
+    TagGroup,
+    Toast,
+    Typography,
+    Button,
+    ButtonGroup,
+    Col,
+    Row,
+    Space,
+    SideSheet,
+    Form,
+    Table,
+    Avatar
+} from "@douyinfe/semi-ui";
+import {AddGift, DelGift, QueryThingList} from "../../api/admin";
+import {getDate, getQueryJson} from "../../utils";
 
 export default function Dashboard() {
     const navi = useNavigate();
     const [addPanel, setAddPanel] = useState(false);
     const {Title} = Typography;
     const [newItem, setNewItem] = useState({});
+    const [itemList, updateItemList] = useState([]);
 
+    /**
+     * 表格列定义
+     * @type {[{dataIndex: string, title: string, render: (function(*, *, *))},{dataIndex: string, title: string},{dataIndex: string, title: string},{dataIndex: string, title: string, render: (function(*))},{dataIndex: string, title: string, render: (function(*, *, *))},null]}
+     */
+    const columns = [
+        {
+            title: '标题',
+            dataIndex: 'title',
+            render: (text, record, index) => {
+                return (
+                    <div>
+                        <Avatar size="small" shape="square" src={record.path_img} style={{ marginRight: 12 }}></Avatar>
+                        {text}
+                    </div>
+                );
+            }
+        },
+        {
+            title: '数量',
+            dataIndex: 'num',
+        },
+        {
+            title: '分类',
+            dataIndex: 'kind',
+
+        },
+        {
+            title: '标签',
+            dataIndex: 'labels',
+            render: (text) => {
+                const arr = []
+                text.slice(1, -1).split('#').forEach(e => {
+                    arr.push({children: e})
+                })
+
+                return (<div><TagGroup tagList={arr}/></div>)
+            }
+        },
+        {
+            title: "发布时间",
+            dataIndex: 'created_at',
+            render: (text, record, index) => {
+                return (
+                    <div>
+                        {getDate(text)}
+                    </div>
+                );
+            }
+        },
+        {
+            title: '',
+            dataIndex: 'operate',
+            render: (text, record, index) => {
+                return (
+                    <ButtonGroup>
+                        <Button type={"primary"} icon={<IconPlus />}/>
+                        <Button icon={<IconMinus />}/>
+                        <Button type={"danger"} icon={<IconDelete />} onClick={() => handleDelete(record, index)}/>
+                    </ButtonGroup>
+                );
+            }
+        },
+    ];
+
+    /**
+     * 判断登录态
+     */
     useEffect(() => {
         if (!localStorage.getItem('access_token')) {
             Toast.info({
@@ -27,8 +109,29 @@ export default function Dashboard() {
                 duration: 2
             })
         }
-    }, [])
+        // eslint-disable-next-line
+    }, []);
 
+    /**
+     * 获取礼品列表
+     */
+    useEffect(() => {
+        QueryThingList().then(res => {
+            if(res.data.code !== 200) {
+                Toast.info({
+                    content: "礼品列表获取出错",
+                    duration: 2
+                })
+            } else {
+                updateItemList(res.data.data)
+            }
+        })
+    }, []);
+
+    /**
+     * 参数判断
+     * @returns {string|boolean}
+     */
     const paramCheck = () => {
         const keys = ['title', 'description', 'kind', 'path_img', 'labels', 'num']
         if (!keys.every((key) => Object.hasOwn(newItem, key)))
@@ -36,6 +139,9 @@ export default function Dashboard() {
         return false
     }
 
+    /**
+     * 确认添加物品回调
+     */
     const handleSubmit = () => {
         if (!paramCheck()) {
             newItem.labels = '#' + newItem.labels.join('#') + '#';
@@ -52,6 +158,27 @@ export default function Dashboard() {
                 duration: 2
             })
         }
+    }
+
+    const handleDelete = (record, index) => {
+        DelGift(record.id).then(res => {
+            console.log(res.data)
+            if (res.data.code === 200) {
+                updateItemList(itemList.filter((_, i) => i !== index))
+                Toast.success({
+                    content: "删除成功",
+                    duration: 2
+                })
+            }
+            else
+            {
+                Toast.error({
+                    content: res.data.msg,
+                    duration: 2
+                })
+            }
+        })
+
     }
 
     const footer = (
@@ -80,6 +207,13 @@ export default function Dashboard() {
                     </Col>
                 </Row>
             </div>
+
+            <div className="thing-list">
+                {
+                    itemList && <Table columns={columns} dataSource={itemList}/>
+                }
+            </div>
+
             <SideSheet title={"添加礼品"} keepDOM={true} footer={footer} visible={addPanel} onCancel={() => {
                 setAddPanel(false)
             }}>
