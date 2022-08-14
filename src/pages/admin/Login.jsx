21c8d4ded5968c3ef24855aab848534f540453f9
youranreus <youranreus@qq.com>
2022-08-14 15:35:17 +0800

    feat: 登录增加用户名字段保存

--- src/pages/admin/Login.jsx
+++ src/pages/admin/Login.jsx
@@ -32,6 +32,7 @@ export default function Login () {
                     })
                     localStorage.setItem('access_token', res.data.access_token);
                     localStorage.setItem('refresh_token', res.data.refresh_token);
+                    localStorage.setItem('user_id', username);
                     setLoading(false);
                     navi('/admin');
                 }
