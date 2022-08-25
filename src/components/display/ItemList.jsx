c5190f38a6e19317f97976f94d3c31b595b6b3ea
youranreus <youranreus@qq.com>
2022-08-26 00:20:54 +0800

    feat: 加载动画

--- src/components/display/ItemList.jsx
+++ src/components/display/ItemList.jsx
@@ -2,6 +2,9 @@
  * @author 季悠然
  * @date 2022-08-17
  */
-export default function ItemList() {
+export default function ItemList(props) {
 
+    return (<div className={"item-list"}>
+
+    </div>)
 }
--- src/pages/display/Index.jsx
+++ src/pages/display/Index.jsx
@@ -5,16 +5,27 @@
 import Filter from "../../components/filter";
 import {useEffect, useState} from "react";
 import {QueryThingList} from "../../api/gift";
+import {Spin, Toast} from "@douyinfe/semi-ui";
+import { IconLoading } from '@douyinfe/semi-icons';
 
 function Index() {
     const [filterObj, setFilterObj] = useState({});
+    const [itemList, updateItemList] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     /**
      * 获取礼物列表
      */
     useEffect(()=> {
+        setLoading(true);
         QueryThingList(filterObj).then(res => {
-            console.log(res);
+            if (res.data.code === 200) {
+                updateItemList(res.data.data);
+                setLoading(false);
+            }
+            else throw new Error(res.data.msg);
+        }).catch(err => {
+            Toast.error(err.message);
         })
     }, [filterObj]);
 
@@ -30,7 +41,9 @@ function Index() {
         <div className="index">
             <Filter callback={getFilterObject}/>
             <div className="content">
-
+                <div className="loading">
+                    <Spin indicator={<IconLoading />} size={"large"} spinning={loading}/>
+                </div>
             </div>
         </div>
     );
