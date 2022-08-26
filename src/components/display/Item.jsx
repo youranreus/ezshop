d5eb4e82a81e52c6527021202cecb3e96deda116
youranreus <youranreus@qq.com>
2022-08-26 17:23:13 +0800

    feat: 礼品详情

--- src/components/display/Item.jsx
+++ src/components/display/Item.jsx
@@ -2,12 +2,13 @@
  * @author 季悠然
  * @date 2022-08-07
  */
-import {Card, Col, Row, Skeleton} from '@douyinfe/semi-ui';
+import {Card, Col, Row, Skeleton, SideSheet, Space, Tag, Divider} from '@douyinfe/semi-ui';
 import {useEffect, useState} from "react";
 
 export default function Item(props) {
     const {itemData} = props;
     const [loading, setLoading] = useState(true);
+    const [showDetail, setShowDetail] = useState(false);
 
     /**
      * 图片懒加载
@@ -25,29 +26,72 @@ export default function Item(props) {
     }, []);
 
     return (<div className={"item"}>
-        <Card
-            cover={
-                <Skeleton loading={loading} placeholder={<Skeleton.Image/>} active={true}
-                          style={{width: "calc((100vw - 1.5rem) /2)", height: "calc((100vw - 1.5rem) /2)"}}>
+        <div onClick={() => setShowDetail(true)}>
+            <Card
+                cover={
+                    <Skeleton loading={loading} placeholder={<Skeleton.Image/>} active={true}
+                              style={{width: "calc((100vw - 1.5rem) /2)", height: "calc((100vw - 1.5rem) /2)"}}>
+                        <img
+                            alt={itemData.title}
+                            src={itemData.path_img}
+                        />
+                    </Skeleton>
+                }
+                bodyStyle={{padding: '8px'}}
+            >
+                <h4>{itemData.title}</h4>
+                <div className="meta">
+                    <Row>
+                        <Col span={12}>
+                            <span className={"kind"}># {itemData.kind}</span>
+                        </Col>
+                        <Col span={12} style={{textAlign: "right"}}>
+                            <span className={"price"}>￥{itemData.ori_price}</span>
+                        </Col>
+                    </Row>
+                </div>
+            </Card>
+        </div>
+
+        <SideSheet title={"礼品详情"} visible={showDetail} height={"90vh"} placement={"bottom"} onCancel={() => setShowDetail(false)}>
+            <div className="detail" style={{maxWidth: "580px", margin: "0 auto"}}>
+                <Skeleton loading={loading} placeholder={<Skeleton.Image/>} active={true}>
                     <img
                         alt={itemData.title}
                         src={itemData.path_img}
                     />
                 </Skeleton>
-            }
-            bodyStyle={{padding: '8px'}}
-        >
-            <h4>{itemData.title}</h4>
-            <div className="meta">
-                <Row>
-                    <Col span={12}>
-                        <span className={"kind"}># {itemData.kind}</span>
-                    </Col>
-                    <Col span={12} style={{textAlign: "right"}}>
-                        <span className={"price"}>￥{itemData.ori_price}</span>
-                    </Col>
-                </Row>
+                <h4>{itemData.title}</h4>
+
+                <div className="meta">
+                    <Row>
+                        <Col span={12}>
+                            <span className={"kind"}># {itemData.kind}</span>
+                        </Col>
+                        <Col span={12} style={{textAlign: "right"}}>
+                            <span className={"price"}>￥{itemData.ori_price}</span>
+                        </Col>
+                    </Row>
+                    <br/>
+                    <Row>
+                        <Col span={24}>
+                            <Space wrap={true}>
+                                {itemData.labels.slice(1, -1).split('#').map((tag, index) => {
+                                    return <Tag key={index} color={"green"} type={"light"}>{tag}</Tag>
+                                })}
+                            </Space>
+                        </Col>
+                    </Row>
+
+                    <Divider margin='12px'>
+                        详细信息
+                    </Divider>
+
+                    <div className="info">
+                        {itemData.description}
+                    </div>
+                </div>
             </div>
-        </Card>
+        </SideSheet>
     </div>)
 }
